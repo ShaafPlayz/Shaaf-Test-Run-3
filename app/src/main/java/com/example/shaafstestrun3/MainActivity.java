f7fd58b4fd56ad3ad28cda7864f9be6959c32a20
Shaaf <31102081+ShaafPlayz@users.noreply.github.com>
2024-11-17 16:45:54 -0500

    login page

--- app/src/main/java/com/example/shaafstestrun3/MainActivity.java
+++ app/src/main/java/com/example/shaafstestrun3/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.shaafstestrun3;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button click = (Button) findViewById(R.id.existing);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.loginpage);
+            }
+        });
+
+//        public void loginpage() {
+//            Button back = (Button) findViewById(R.id.backbutton);
+//            back.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    setContentView(R.layout.activity_main);
+//                }
+//            });
+//        }
     }
 }
